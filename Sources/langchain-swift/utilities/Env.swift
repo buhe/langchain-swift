081bd1c6a93436b7b63bcc4e1a8779c4723a2e5d
buhe <bugu1986@126.com>
2023-06-11 13:58:54 +0800

    remove //

--- Sources/langchain-swift/utilities/Env.swift
+++ Sources/langchain-swift/utilities/Env.swift
@@ -15,16 +15,15 @@ func loadEnv() -> [String: String] {
         let envContent = try String(contentsOfFile: envPath)
         // 拆分字符串成行
         let envLines = envContent.components(separatedBy: .newlines)
-        // 遍历每一行，解析环境变量和值并设置到 ProcessInfo 中
         for line in envLines {
-            // 忽略注释行
+
             if line.hasPrefix("#") { continue }
-            // 拆分为环境变量和值
+
             let parts = line.components(separatedBy: "=")
             guard parts.count == 2 else { continue }
             let key = parts[0].trimmingCharacters(in: .whitespaces)
             let value = parts[1].trimmingCharacters(in: .whitespaces)
-            // 设置到 ProcessInfo 中
+
             env[key] = value
         }
     } catch {
