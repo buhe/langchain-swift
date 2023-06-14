6ea99f11e7d05eb99875428170fa2f17550f56d0
buhe <bugu1986@126.com>
2023-06-14 17:04:42 +0800

    u

---
+++ Sources/langchain-swift/vectorstores/supabase.sql
@@ -0,0 +1,27 @@
+
+
+-- Create a table to store your documents
+create table documents (
+  id bigserial primary key,
+  content text,  -- corresponds to Document.pageContent
+  embedding vector(1536)  -- 1536 works for OpenAI embeddings, change if needed
+);
+
+-- Create a function to search for documents
+create function match_documents(query_embedding vector(1536), match_count int)
+returns table(id bigint, content text, similarity float)
+language plpgsql
+as $$
+#variable_conflict use_column
+begin
+  return query
+  select
+    id,
+    content,
+    1 - (documents.embedding <=> query_embedding) as similarity
+  from documents
+  order by documents.embedding <=> query_embedding
+  limit match_count;
+end;
+$$
+;
