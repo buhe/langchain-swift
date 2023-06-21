91b52c4d17d49846b4a68afc0c255101f284311e
buhe <bugu1986@126.com>
2023-06-21 16:28:44 +0800

    Update langchain_swiftTests.swift

--- Tests/langchain-swiftTests/langchain_swiftTests.swift
+++ Tests/langchain-swiftTests/langchain_swiftTests.swift
@@ -1,12 +1,15 @@
 import XCTest
 @testable import langchain_swift
+@testable import LangChain
 
 final class langchain_swiftTests: XCTestCase {
-    func testExample() throws {
+    func testFormat() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
 
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        let s1 = PromptTemplate(input_variables: ["1", "2"], template: SUFFIX).format(args: [ "dog" , "cat"] )
+        XCTAssertNotNil(s1)
     }
 }
