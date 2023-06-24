4384a2c0464788638437ca484e5c3d968d5dc2be
buhe <bugu1986@126.com>
2023-06-24 13:55:46 +0800

    "Create BaseLoader and TextLoader structs for loading documents."

---
+++ Sources/LangChain/document_loaders/BaseLoader.swift
@@ -0,0 +1,52 @@
+//
+//  File.swift
+//  
+//
+//  Created by 顾艳华 on 2023/6/24.
+//
+
+import Foundation
+public struct Document {
+    let page_content: String
+    let metadata: [String: String]
+}
+public protocol BaseLoader {
+    func load() -> [Document]
+}
+//class BaseLoader(ABC):
+//    """Interface for loading documents.
+//
+//    Implementations should implement the lazy-loading method using generators
+//    to avoid loading all documents into memory at once.
+//
+//    The `load` method will remain as is for backwards compatibility, but its
+//    implementation should be just `list(self.lazy_load())`.
+//    """
+//
+//    # Sub-classes should implement this method
+//    # as return list(self.lazy_load()).
+//    # This method returns a List which is materialized in memory.
+//    @abstractmethod
+//    def load(self) -> List[Document]:
+//        """Load data into document objects."""
+//
+//    def load_and_split(
+//        self, text_splitter: Optional[TextSplitter] = None
+//    ) -> List[Document]:
+//        """Load documents and split into chunks."""
+//        if text_splitter is None:
+//            _text_splitter: TextSplitter = RecursiveCharacterTextSplitter()
+//        else:
+//            _text_splitter = text_splitter
+//        docs = self.load()
+//        return _text_splitter.split_documents(docs)
+//
+//    # Attention: This method will be upgraded into an abstractmethod once it's
+//    #            implemented in all the existing subclasses.
+//    def lazy_load(
+//        self,
+//    ) -> Iterator[Document]:
+//        """A lazy loader for document content."""
+//        raise NotImplementedError(
+//            f"{self.__class__.__name__} does not implement lazy_load()"
+//        )
---
+++ Sources/LangChain/document_loaders/TextLoader.swift
@@ -0,0 +1,79 @@
+//
+//  File.swift
+//  
+//
+//  Created by 顾艳华 on 2023/6/24.
+//
+
+import Foundation
+
+public struct TextLoader: BaseLoader {
+    let file_path: String
+    
+    public init(file_path: String) {
+        self.file_path = file_path
+    }
+    public func load() -> [Document] {
+        let nameAndExt = self.file_path.split(separator: ".")
+        let name = "\(nameAndExt[0])"
+        let ext = "\(nameAndExt[1])"
+        var text = ""
+        let res = Bundle.main.path(forResource: name, ofType: ext)!
+        do {
+            text = try String(contentsOfFile: res)
+        } catch {
+            print("Unable to load res file: \(error)")
+        }
+        let metadata = ["source": self.file_path]
+        return [Document(page_content: text, metadata: metadata)]
+    }
+}
+//class TextLoader(BaseLoader):
+//    """Load text files.
+//
+//
+//    Args:
+//        file_path: Path to the file to load.
+//
+//        encoding: File encoding to use. If `None`, the file will be loaded
+//        with the default system encoding.
+//
+//        autodetect_encoding: Whether to try to autodetect the file encoding
+//            if the specified encoding fails.
+//    """
+//
+//    def __init__(
+//        self,
+//        file_path: str,
+//        encoding: Optional[str] = None,
+//        autodetect_encoding: bool = False,
+//    ):
+//        """Initialize with file path."""
+//        self.file_path = file_path
+//        self.encoding = encoding
+//        self.autodetect_encoding = autodetect_encoding
+//
+//    def load(self) -> List[Document]:
+//        """Load from file path."""
+//        text = ""
+//        try:
+//            with open(self.file_path, encoding=self.encoding) as f:
+//                text = f.read()
+//        except UnicodeDecodeError as e:
+//            if self.autodetect_encoding:
+//                detected_encodings = detect_file_encodings(self.file_path)
+//                for encoding in detected_encodings:
+//                    logger.debug("Trying encoding: ", encoding.encoding)
+//                    try:
+//                        with open(self.file_path, encoding=encoding.encoding) as f:
+//                            text = f.read()
+//                        break
+//                    except UnicodeDecodeError:
+//                        continue
+//            else:
+//                raise RuntimeError(f"Error loading {self.file_path}") from e
+//        except Exception as e:
+//            raise RuntimeError(f"Error loading {self.file_path}") from e
+//
+//        metadata = {"source": self.file_path}
+//        return [Document(page_content=text, metadata=metadata)]
