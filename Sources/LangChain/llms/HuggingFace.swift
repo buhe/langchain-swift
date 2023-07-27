16165c97b35018a52d95bb95e6b603b94fd9e2d7
buhe <bugu1986@126.com>
2023-07-27 16:22:17 +0800

    null

---
+++ Sources/LangChain/llms/HuggingFace.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by 顾艳华 on 2023/7/27.
+//
+
+import Foundation
+public struct HuggingFace: LLM {
+    public func send(text: String, stops: [String]) async -> String {
+        ""
+    }
+    
+    func buildHeader() {
+        
+    }
+    
+    func buildPayload() {
+        
+    }
+    
+}
---
+++ Sources/LangChain/utilities/InferenceApi.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  
+//
+//  Created by 顾艳华 on 2023/7/27.
+//
+
+import Foundation
+struct InferenceApi {
+    
+}
