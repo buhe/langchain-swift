d5ab9291974a91d982405b04af9cce03e7753e2d
buhe <bugu1986@126.com>
2023-07-31 19:52:04 +0800

    "Add BilibiliLoader's initializer and implement the load() function using BilibiliClient to fetch video info."

--- Sources/LangChain/document_loaders/BilibiliLoader.swift
+++ Sources/LangChain/document_loaders/BilibiliLoader.swift
@@ -10,6 +10,10 @@ import Foundation
 public struct BilibiliLoader: BaseLoader {
     let videoId: String
     
+    public init(videoId: String) {
+        self.videoId = videoId
+    }
+        
     public func load() async -> [Document] {
         let client = BilibiliClient(credential: BilibiliCredential(sessin: "6376fa3e%2C1705926902%2C0b561%2A71gvy_TPyZMWhUweKjYGT502_5FVZdcv8bfjvwtqdlqm8UjyEiUrkPq1AodolcSjIgBXatNwAAEgA", jct: "330aaac577464e453ea1b070fd1281ea"))
         let info = await client.fetchVideoInfo(bvid: videoId)
