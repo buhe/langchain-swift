6e66870801ddd18f0dd1fcc885508a770f86780f
buhe <bugu1986@126.com>
2023-08-02 12:49:32 +0800

    "Refactor BilibiliLoader.swift to handle nil video info."

--- Sources/LangChain/document_loaders/BilibiliLoader.swift
+++ Sources/LangChain/document_loaders/BilibiliLoader.swift
@@ -17,6 +17,9 @@ public struct BilibiliLoader: BaseLoader {
     public func load() async -> [Document] {
         let client = BilibiliClient(credential: BilibiliCredential(sessin: "6376fa3e%2C1705926902%2C0b561%2A71gvy_TPyZMWhUweKjYGT502_5FVZdcv8bfjvwtqdlqm8UjyEiUrkPq1AodolcSjIgBXatNwAAEgA", jct: "330aaac577464e453ea1b070fd1281ea"))
         let info = await client.fetchVideoInfo(bvid: videoId)
+        if info == nil {
+            return []
+        }
         return [Document(page_content: info!.subtitle, metadata: [
             "title": info!.title,
             "desc": info!.desc,
