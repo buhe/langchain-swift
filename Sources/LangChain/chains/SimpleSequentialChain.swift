52a7c2f30476b47bf53b04e49746fde53009e37a
buhe <bugu1986@126.com>
2023-09-06 10:50:38 +0800

    Create SimpleSequentialChain.swift

---
+++ Sources/LangChain/chains/SimpleSequentialChain.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by 顾艳华 on 2023/9/6.
+//
+
+import Foundation
+public class SimpleSequentialChain: DefaultChain {
+    public override func call(args: String) async throws -> LLMResult {
+        
+        return LLMResult()
+    }
+}
