6109541eeacdd382ab25dedc488a7363e1b4fe2c
buhe <bugu1986@126.com>
2023-09-07 20:40:48 +0800

    Create Llama2.swift

---
+++ Sources/LangChain/llms/Llama2.swift
@@ -0,0 +1,52 @@
+//
+//  File.swift
+//  
+//
+//  Created by 顾艳华 on 2023/9/7.
+//
+
+
+
+// https://www.llama-api.com/account/api-token
+import Foundation
+import NIOPosix
+import AsyncHTTPClient
+import OpenAIKit
+
+public struct Llama2: LLM {
+    
+    let temperature: Double
+    let model: ModelID
+    
+    public init(temperature: Double = 0.0, model: ModelID = Model.GPT3.gpt3_5Turbo) {
+        self.temperature = temperature
+        self.model = model
+    }
+    
+    public func send(text: String, stops: [String] = []) async -> LLMResult {
+        let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+
+        let httpClient = HTTPClient(eventLoopGroupProvider: .shared(eventLoopGroup))
+       
+        let env = Env.loadEnv()
+        
+        if let apiKey = env["LLAMA2_API_KEY"] {
+
+            let configuration = Configuration(apiKey: apiKey, api: API(scheme: .https, host: "https://api.llama-api.com"))
+
+            let openAIClient = OpenAIKit.Client(httpClient: httpClient, configuration: configuration)
+            defer {
+                // it's important to shutdown the httpClient after all requests are done, even if one failed. See: https://github.com/swift-server/async-http-client
+                try? httpClient.syncShutdown()
+            }
+            let completion = try! await openAIClient.chats.create(model: model, messages: [.user(content: text)], temperature: temperature, stops: stops)
+            return LLMResult(llm_output: completion.choices.first!.message.content)
+        } else {
+            print("Please set openai api key.")
+            return LLMResult(llm_output: "Please set openai api key.")
+        }
+        
+    }
+    
+    
+}
