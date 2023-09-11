454f003ebdaba9837129c9108127d09980fb3e04
buhe <bugu1986@126.com>
2023-09-11 16:17:45 +0800

    "Add fetchIDIfExist() function to ReportManager and implement saving env.txt on dev env."

--- Sources/LangChain/utilities/report/ReportManager.swift
+++ Sources/LangChain/utilities/report/ReportManager.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 struct Report {
-    
+    let appDisplayName: String?
+    let id: String
     let sended: Bool = false
 }
 
@@ -17,11 +18,22 @@ struct ReportManager {
     
     static let shared: ReportManager = ReportManager()
     
+    func fetchIDIfExist() -> String {
+        let id = UUID().uuidString
+        // TODO: save env.txt on dev env
+        return id
+    }
     mutating func insertReport(report: Report) {
         reports.append(report)
     }
     
     func sendServer() {
         
+    }
+}
+
+extension Bundle {
+    var appDisplayName: String? {
+        return infoDictionary?["CFBundleDisplayName"] as? String
     }
 }
