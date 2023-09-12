228810c0eebe431c4869057d772ddc4ca25b844f
buhe <bugu1986@126.com>
2023-09-12 18:27:55 +0800

    "Refactor ReportManager.swift to include a new property `reportId` and a new method `sendServer()` to send reports to the server."

--- Sources/LangChain/utilities/report/ReportManager.swift
+++ Sources/LangChain/utilities/report/ReportManager.swift
@@ -9,22 +9,22 @@ import Foundation
 
 struct Report {
     let appDisplayName: String?
-    let id: String
+    let reportId: String
+    let type: String
+    let message: String
+    let success: Bool
     let sended: Bool = false
 }
 
 struct ReportManager {
     var reports: [Report] = []
-    
+    static let REPORT_URL = "http://127.0.0.1:8083/rest/agent"
     static let shared: ReportManager = ReportManager()
     
-    func fetchIDIfExist() -> String {
-        let id = UUID().uuidString
-        // TODO: save env.txt on dev env
-        return id
-    }
     mutating func insertReport(report: Report) {
         reports.append(report)
+        
+        sendServer()
     }
     
     func sendServer() {
