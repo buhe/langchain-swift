5186ea58887149d14df6fab6915264d7ea4e177a
buhe <bugu1986@126.com>
2023-11-02 21:40:25 +0800

    "Add BaseRetriever and WikipediaRetriever classes for document retrieval."

---
+++ Sources/LangChain/retriever/BaseRetriever.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by 顾艳华 on 2023/11/1.
+//
+
+import Foundation
+
+public class BaseRetriever {
+    public func get_relevant_documents(query: String) -> [Document] {
+        []
+    }
+}
---
+++ Sources/LangChain/retriever/WikipediaRetriever.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  
+//
+//  Created by 顾艳华 on 2023/11/2.
+//
+
+import Foundation
+public class WikipediaRetriever: BaseRetriever {
+    
+}
