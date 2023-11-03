30507386ee4ecc3bced89f2b4dd5a25a529df9a8
buhe <bugu1986@126.com>
2023-11-03 16:00:40 +0800

    "Refactor WikipediaAPIWrapper to use the new WikipediaPage struct and implement the search function."

---
+++ Sources/LangChain/utilities/wikipedia/WikipediaPage.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by 顾艳华 on 2023/11/3.
+//
+
+import Foundation
+struct WikipediaPage {
+    let title: String
+    let pageid: Int
+    
+    func content() -> String {
+        ""
+    }
+}
