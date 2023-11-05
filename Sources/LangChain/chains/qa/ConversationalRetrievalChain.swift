fdbcdbed7d8911a9ec6db42bd5557d871b57950a
buhe <bugu1986@126.com>
2023-11-05 14:46:43 +0800

    "Collect docs: \"Lorem ipsu...\" 1234 count"

--- Sources/LangChain/chains/qa/ConversationalRetrievalChain.swift
+++ Sources/LangChain/chains/qa/ConversationalRetrievalChain.swift
@@ -16,7 +16,7 @@ public class ConversationalRetrievalChain: BaseConversationalRetrievalChain {
     override func get_docs(question: String) async -> String {
         let docs = await retriver.get_relevant_documents(query: question)
         let docsStr = docs.map{$0.page_content}.joined(separator: "\n\n")
-        print("🦙docs: \(docsStr.prefix(10))...")
+        print("🦙>>Collect docs: \(docsStr.prefix(10))... \(docsStr.count) count")
         return "\(docsStr)"
     }
 }
