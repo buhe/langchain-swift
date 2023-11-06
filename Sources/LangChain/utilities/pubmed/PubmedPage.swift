b1da2c9cda53dc5c9f22a3cdcbb00abb8505e866
buhe <bugu1986@126.com>
2023-11-06 16:18:14 +0800

    "Refactor PubmedAPIWrapper and PubmedPage to use the PubMed API for searching and retrieving content."

--- Sources/LangChain/utilities/pubmed/PubmedPage.swift
+++ Sources/LangChain/utilities/pubmed/PubmedPage.swift
@@ -7,44 +7,44 @@
 
 import AsyncHTTPClient
 import Foundation
-import SwiftyJSON
+import SWXMLHash
 import NIOPosix
 
 struct PubmedPage {
+    let uid: String
+    let webenv: String
     
     func content() async throws -> String {
-        ""
-//        let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-//        let httpClient = HTTPClient(eventLoopGroupProvider: .shared(eventLoopGroup))
-//        defer {
-//            // it's important to shutdown the httpClient after all requests are done, even if one failed. See: https://github.com/swift-server/async-http-client
-//            try? httpClient.syncShutdown()
-//        }
-//        
-//        let baseURL = "http://en.wikipedia.org/w/api.php"
-//        var components = URLComponents(string: baseURL)!
-//        components.queryItems = [
-//            URLQueryItem(name: "prop", value: "extracts|revisions"),
-//            URLQueryItem(name: "rvprop", value: "ids"),
-//            URLQueryItem(name: "titles", value: self.title),
-//            URLQueryItem(name: "action", value: "query"),
-//            URLQueryItem(name: "format", value: "json"),
-//        ]
-////        print(components.url!.absoluteString)
-//        var request = HTTPClientRequest(url: components.url!.absoluteString)
-//        request.method = .GET
-//        
-//        let response = try await httpClient.execute(request, timeout: .seconds(30))
-//        if response.status == .ok {
-//            let str = String(buffer: try await response.body.collect(upTo: 1024 * 1024))
-////            print(str)
-//            let json = try JSON(data: str.data(using: .utf8)!)
-//            return json["query"]["pages"]["\(self.pageid)"]["extract"].stringValue
-//            
-//        } else {
-//            // handle remote error
-//            print("http code is not 200.")
-//            return ""
-//        }
+        let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        let httpClient = HTTPClient(eventLoopGroupProvider: .shared(eventLoopGroup))
+        defer {
+            // it's important to shutdown the httpClient after all requests are done, even if one failed. See: https://github.com/swift-server/async-http-client
+            try? httpClient.syncShutdown()
+        }
+        
+        let baseURL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
+        var components = URLComponents(string: baseURL)!
+        components.queryItems = [
+            URLQueryItem(name: "db", value: "pubmed"),
+            URLQueryItem(name: "retmode", value: "xml"),
+            URLQueryItem(name: "id", value: self.uid),
+            URLQueryItem(name: "webenv", value: self.webenv),
+        ]
+        print(components.url!.absoluteString)
+        var request = HTTPClientRequest(url: components.url!.absoluteString)
+        request.method = .GET
+        
+        let response = try await httpClient.execute(request, timeout: .seconds(30))
+        if response.status == .ok {
+            let str = String(buffer: try await response.body.collect(upTo: 1024 * 1024))
+            let xml = XMLHash.parse(str.data(using: .utf8)!)
+            let summary = xml["PubmedArticleSet"].element!.text
+            print("xml: \(summary)")
+            return summary
+        } else {
+            // handle remote error
+            print("http code is not 200.")
+            return ""
+        }
     }
 }
