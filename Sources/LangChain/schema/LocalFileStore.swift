94cca56f09a22c1d277713d9d09ffd0d8723bf70
buhe <bugu1986@126.com>
2023-11-19 12:38:59 +0800

    "Refactor LocalFileStore to use SwiftFileStore and add StoreEntry struct for better organization and readability."

--- Sources/LangChain/schema/LocalFileStore.swift
+++ Sources/LangChain/schema/LocalFileStore.swift
@@ -6,6 +6,35 @@
 //
 
 import Foundation
+import SwiftFileStore
+struct StoreEntry: Codable, JSONDataRepresentable {
+    let key: String
+    let value: String
+}
 public class LocalFileStore: BaseStore {
+    let objectStore: FileObjectStore?
+
+    public override init() {
+        do {
+            self.objectStore = try FileObjectStore.create()
+        } catch {
+            self.objectStore = nil
+        }
+    }
     
+    override func mget(keys: [String]) -> [String] {
+        []
+    }
+    
+    override func mset(kvpairs: [(String, String)]) {
+        
+    }
+    
+    override func mdelete(keys: [String]) {
+        
+    }
+    
+    override func keys(prefix: String? = nil) -> [String] {
+        []
+    }
 }
