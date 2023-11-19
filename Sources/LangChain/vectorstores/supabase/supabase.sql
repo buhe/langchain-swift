95b31e372c3b249bbdf829a4e4ad2bfebd896597
buhe <bugu1986@126.com>
2023-11-19 11:36:58 +0800

    "Update embedding column definition in supabase.sql to include a comma."

--- Sources/LangChain/vectorstores/supabase/supabase.sql
+++ Sources/LangChain/vectorstores/supabase/supabase.sql
@@ -4,7 +4,7 @@
 create table documents (
   id bigserial primary key,
   content text,  -- corresponds to Document.pageContent
-  embedding vector(1536)  -- 1536 works for OpenAI embeddings, change if needed
+  embedding vector(1536),  -- 1536 works for OpenAI embeddings, change if needed
   metadata jsonb
 );
 
