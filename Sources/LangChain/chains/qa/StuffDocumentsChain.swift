a5c491bf110dd2c69bdf38548ef7ce43094f7cb9
buhe <bugu1986@126.com>
2023-11-25 15:15:05 +0800

    "Refactor StuffDocumentsChain.swift to include a more detailed prompt template for answering questions, including the original text on which the answer depends."

--- Sources/LangChain/chains/qa/StuffDocumentsChain.swift
+++ Sources/LangChain/chains/qa/StuffDocumentsChain.swift
@@ -10,12 +10,17 @@ import Foundation
 public class StuffDocumentsChain: BaseCombineDocumentsChain {
     
     static let prompt_template = """
-    Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
-    
-    {context}
+    Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer, And give the original text on which the answer depends.
+    Use the following format:
+    Context: pieces of context to answer
+    Question: the input question you must answer
+    Helpful Answer: the final answer to the original input question
+    Dependent text: the original text on which the answer depends
 
+    Begin!
+
+    Context: {context}
     Question: {question}
-    Helpful Answer:
 """
     static let PROMPT = PromptTemplate(input_variables: ["context", "question"], partial_variable: [:], template: prompt_template)
 
