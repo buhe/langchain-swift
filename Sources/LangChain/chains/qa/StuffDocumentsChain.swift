4a5385ab1d0789d65ad89ad8ed6ca80d4012f034
buhe <bugu1986@126.com>
2023-12-06 17:38:39 +0800

    "Refactor StuffDocumentsChain.swift to include instructions for using context language when answering questions and emphasize not making up answers."

--- Sources/LangChain/chains/qa/StuffDocumentsChain.swift
+++ Sources/LangChain/chains/qa/StuffDocumentsChain.swift
@@ -10,7 +10,7 @@ import Foundation
 public class StuffDocumentsChain: BaseCombineDocumentsChain {
     
     static let prompt_template = """
-    Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer, And give the original text on which the answer depends.
+    Use the following pieces of context to answer the question at the end, use context language. If you don't know the answer, just say that you don't know, don't try to make up an answer, And give the original text on which the answer depends.
     Use the following format:
     Context: pieces of context to answer
     Question: the input question you must answer
