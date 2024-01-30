a1c64701e57893dfa26f34d705e76b56c50d78c1
buhe <bugu1986@126.com>
2024-01-30 11:26:53 +0800

    "Add llmfarm_core.swift as a remote source control dependency with the branch \"langchain\" and revision \"927d670751bc8aebbc5eb845afd36fe1eeef4f5a\", and update the Package.swift file to include the llmfarm_core product."

--- Sources/LangChain/llms/Local.swift
+++ Sources/LangChain/llms/Local.swift
@@ -1,46 +1,46 @@
-////
-////  File.swift
-////  
-////
-////  Created by 顾艳华 on 1/22/24.
-////
-//import llmfarm_core
-//import Foundation
 //
-//public class Local: LLM {
-//    let modelPath: String
-//    let useMetal: Bool
-//    let inference: ModelInference
-//    
-//    public init(inference: ModelInference, modelPath: String, useMetal: Bool = false, callbacks: [BaseCallbackHandler] = [], cache: BaseCache? = nil) {
-//        self.inference = inference
-//        self.modelPath = modelPath
-//        self.useMetal = useMetal
-//        super.init(callbacks: callbacks, cache: cache)
-//    }
-//    public override func _send(text: String, stops: [String] = []) async throws -> LLMResult {
-//        let ai = AI(_modelPath: self.modelPath, _chatName: "chat")
-//        var params:ModelAndContextParams = .default
-//        params.use_metal = useMetal
-//        params.promptFormat = .Custom
-//        params.custom_prompt_format = "{{prompt}}"
-//        try? ai.loadModel(inference, contextParams: params)
-//        let output = try? ai.model.predict(text, mainCallback)
-////        print("🚗\(output)")
-//        total_output = 0
-//        return LLMResult(llm_output: output)
-//    }
-//    
-//    let maxOutputLength = 256
-//    var total_output = 0
+//  File.swift
+//  
 //
-//    func mainCallback(_ str: String, _ time: Double) -> Bool {
-//        print("\(str)",terminator: "")
-//        total_output += str.count
-//        if(total_output>maxOutputLength){
-//            return true
-//        }
-//        return false
-//    }
-//}
+//  Created by 顾艳华 on 1/22/24.
 //
+import llmfarm_core
+import Foundation
+
+public class Local: LLM {
+    let modelPath: String
+    let useMetal: Bool
+    let inference: ModelInference
+    
+    public init(inference: ModelInference, modelPath: String, useMetal: Bool = false, callbacks: [BaseCallbackHandler] = [], cache: BaseCache? = nil) {
+        self.inference = inference
+        self.modelPath = modelPath
+        self.useMetal = useMetal
+        super.init(callbacks: callbacks, cache: cache)
+    }
+    public override func _send(text: String, stops: [String] = []) async throws -> LLMResult {
+        let ai = AI(_modelPath: self.modelPath, _chatName: "chat")
+        var params:ModelAndContextParams = .default
+        params.use_metal = useMetal
+        params.promptFormat = .Custom
+        params.custom_prompt_format = "{{prompt}}"
+        try? ai.loadModel(inference, contextParams: params)
+        let output = try? ai.model.predict(text, mainCallback)
+//        print("🚗\(output)")
+        total_output = 0
+        return LLMResult(llm_output: output)
+    }
+    
+    let maxOutputLength = 256
+    var total_output = 0
+
+    func mainCallback(_ str: String, _ time: Double) -> Bool {
+        print("\(str)",terminator: "")
+        total_output += str.count
+        if(total_output>maxOutputLength){
+            return true
+        }
+        return false
+    }
+}
+
