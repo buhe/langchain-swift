76e2d4b1c66f5783f6b09e77a14cb4527fae6176
buhe <bugu1986@126.com>
2024-02-06 10:14:07 +0800

    Update Local.swift

--- Sources/LangChain/llms/Local.swift
+++ Sources/LangChain/llms/Local.swift
@@ -11,20 +11,23 @@ public class Local: LLM {
     let modelPath: String
     let useMetal: Bool
     let inference: ModelInference
+    var params: ModelAndContextParams
+    var sampleParams: ModelSampleParams
     
-    public init(inference: ModelInference, modelPath: String, useMetal: Bool = false, callbacks: [BaseCallbackHandler] = [], cache: BaseCache? = nil) {
+    public init(inference: ModelInference, modelPath: String, useMetal: Bool = false,params: ModelAndContextParams = .default, sampleParams: ModelSampleParams = .default, callbacks: [BaseCallbackHandler] = [], cache: BaseCache? = nil) {
         self.inference = inference
         self.modelPath = modelPath
         self.useMetal = useMetal
+        self.params = params
+        self.sampleParams = sampleParams
         super.init(callbacks: callbacks, cache: cache)
     }
     public override func _send(text: String, stops: [String] = []) async throws -> LLMResult {
         let ai = AI(_modelPath: self.modelPath, _chatName: "chat")
-        var params:ModelAndContextParams = .default
-        params.use_metal = useMetal
-        params.promptFormat = .Custom
-        params.custom_prompt_format = "{{prompt}}"
-        try? ai.loadModel(inference, contextParams: params)
+        self.params.use_metal = useMetal
+        
+        let _ = try? ai.loadModel(inference, contextParams: self.params)
+        ai.model.sampleParams = self.sampleParams
         let output = try? ai.model.predict(text, mainCallback)
 //        print("🚗\(output)")
         total_output = 0
