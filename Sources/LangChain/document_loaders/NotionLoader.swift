74883aa34d8aa670945586ad069ca41aa82bc658
buhe <bugu1986@126.com>
2024-02-07 12:19:06 +0800

    "Add NotionSwift package version 0.8.0 as a dependency."

---
+++ Sources/LangChain/document_loaders/NotionLoader.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by 顾艳华 on 2/7/24.
+//
+
+import Foundation
+import NotionSwift
+
+public class NotionLoader: BaseLoader {
+    
+    public override func _load() async throws -> [Document] {
+            []
+    }
+}
