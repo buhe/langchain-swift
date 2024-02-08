329d5c88c41065c21a28f1e4a321bdba62ecc69b
buhe <bugu1986@126.com>
2024-02-08 11:50:09 +0800

    "Remove SwiftyNotion package and update NotionLoader.swift to recursively build blocks from Notion API."

--- Sources/LangChain/document_loaders/NotionLoader.swift
+++ Sources/LangChain/document_loaders/NotionLoader.swift
@@ -10,6 +10,21 @@ import SwiftyNotion
 
 public class NotionLoader: BaseLoader {
     
+    fileprivate func buildBlocks(_ notion: NotionAPIGateway, withId: String) async throws {
+        let content = try await notion.retrieveBlockChildren(withId: withId)
+        //        print(content.properties["title"]?.title![0].plainText)
+        print("blocks: \(content)")
+        for block in content {
+            if block.hasChildren {
+                let blockId = block.id.replacingOccurrences(of: "-", with: "")
+                print("blockId: \(blockId)")
+                try await buildBlocks(notion, withId: blockId)
+            } else {
+                //add document
+            }
+        }
+    }
+    
     public override func _load() async throws -> [Document] {
         
 //        let notion = NotionClient(accessKeyProvider: StringAccessKeyProvider(accessKey: "secret_ODO49SlZawEpwsT3Gzfn401iemthmiaeqKIWL1qf6Th"))
@@ -18,10 +33,9 @@ public class NotionLoader: BaseLoader {
 //        notion.page(pageId: pageId) {
 //            print("page: \($0)")
 //        }
+        var docs = [Document]()
         let notion = NotionAPIGateway(secretKey: "secret_ODO49SlZawEpwsT3Gzfn401iemthmiaeqKIWL1qf6Th")
-        let content = try await notion.retrieveBlockChildren(withId: "b3463f9ddbf34788b9720f6ff2fd0603")
-//        print(content.properties["title"]?.title![0].plainText)
-        print("block: \(content)")
+        try await buildBlocks(notion, withId: "dbdaeff6b2954534ae8323d65053df58")
         return []
     }
 }
