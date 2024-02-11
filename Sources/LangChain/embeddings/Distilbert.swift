658f1b53c7c7392a39d8e316b67f66bec7ef7016
buhe <bugu1986@126.com>
2024-02-11 18:21:27 +0800

    Create Distilbert.swift

---
+++ Sources/LangChain/embeddings/Distilbert.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by 顾艳华 on 2/11/24.
+//
+
+import Foundation
+import SimilaritySearchKitDistilbert
+
+public struct Distilbert: Embeddings {
+    let n = DistilbertEmbeddings()
+    public init() {
+        
+    }
+    
+    
+    public func embedQuery(text: String) async -> [Float] {
+        await n.encode(sentence: text)!
+    }
+}
