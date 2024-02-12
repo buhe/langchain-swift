8a515de361affea3e9c77fe2fed7312295bb4a48
buhe <bugu1986@126.com>
2024-02-12 16:20:03 +0800

    "Refactor RSSLoader to handle loading and parsing of RSS feeds, adding support for Atom and JSON feeds as well."

--- Sources/LangChain/document_loaders/RSSLoader.swift
+++ Sources/LangChain/document_loaders/RSSLoader.swift
@@ -6,8 +6,43 @@
 //
 
 import Foundation
-//import FeedKit
+import FeedKit
 
-public class RSSLoader {
+public class RSSLoader: BaseLoader {
+    let url: String
     
+    public init(url: String, callbacks: [BaseCallbackHandler] = []) {
+        self.url = url
+        super.init(callbacks: callbacks)
+    }
+    public override func _load() async throws  -> [Document] {
+        let feedURL = URL(string: url)!
+        let parser = FeedParser(URL: feedURL)
+        let result = parser.parse()
+        switch result {
+        case .success(let feed):
+            
+            // Grab the parsed feed directly as an optional rss, atom or json feed object
+            switch feed {
+            case let .atom(feed):
+                print("🌈atom:\(feed)")
+                return []// Atom Syndication Format Feed Model
+            case let .rss(feed):
+                var content = [Document]()
+                for f in feed.items ?? [] {
+                    content.append(Document(page_content: f.title ?? "", metadata: [:]))
+                }
+                return content// Really Simple Syndication Feed Model
+            case let .json(feed):
+                print("🌈json:\(feed)")
+                return []// JSON Feed Model
+            }
+            
+            
+        case .failure(let error):
+            print(error)
+            return []
+        }
+        
+    }
 }
