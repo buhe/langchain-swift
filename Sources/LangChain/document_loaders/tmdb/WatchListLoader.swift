6eb5da2cd266362ef094dc569e041098598b983c
buhe <bugu1986@126.com>
2024-03-11 10:44:17 +0800

    "Add SwiftyNotion 0.1.5 as a remote source control and include WatchListLoader.swift file."

---
+++ Sources/LangChain/document_loaders/tmdb/WatchListLoader.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by 顾艳华 on 3/11/24.
+//
+
+import Foundation
+public class WatchListLoader: BaseLoader {
+    public override func _load() async throws -> [Document] {
+        var docs: [Document] = []
+        
+        return docs
+    }
+}
