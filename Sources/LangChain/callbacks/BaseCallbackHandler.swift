93e8c1269976a8e22e3df20e1d1ab9cbb647c95f
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-03-16 16:29:06 +0100

    fix: allow third-party module to use langchain agent

--- Sources/LangChain/callbacks/BaseCallbackHandler.swift
+++ Sources/LangChain/callbacks/BaseCallbackHandler.swift
@@ -6,75 +6,79 @@
 //
 
 import Foundation
-public class BaseCallbackHandler: LLMManagerMixin, ChainManagerMixin, CallbackManagerMixin, ToolManagerMixin, LoaderManagerMixin {
+open class BaseCallbackHandler: LLMManagerMixin, ChainManagerMixin, CallbackManagerMixin, ToolManagerMixin, LoaderManagerMixin {
+    
+    public init() {
+        
+    }
     // Loader
-    public func on_loader_start(type: String, metadata: [String : String]) throws {
+    open func on_loader_start(type: String, metadata: [String : String]) throws {
         
     }
     
-    public func on_loader_error(type: String, cause: String, metadata: [String : String]) throws {
+    open func on_loader_error(type: String, cause: String, metadata: [String : String]) throws {
         
     }
     
-    public func on_loader_end(type: String, metadata: [String : String]) throws {
+    open func on_loader_end(type: String, metadata: [String : String]) throws {
         
     }
     
     // Agent
-    public func on_agent_start(prompt: String, metadata: [String : String]) throws {
+    open func on_agent_start(prompt: String, metadata: [String : String]) throws {
         
     }
     
-    public func on_llm_error(error: Error, metadata: [String: String]) throws {
+    open func on_llm_error(error: Error, metadata: [String: String]) throws {
         
     }
     
-    public func on_llm_start(prompt: String, metadata: [String: String]) throws {
+    open func on_llm_start(prompt: String, metadata: [String: String]) throws {
         
     }
     
     // Manage callback
-    public func on_chain_start(prompts: String, metadata: [String: String]) throws {
+    open func on_chain_start(prompts: String, metadata: [String: String]) throws {
         
     }
     
-    public func on_tool_start(tool: BaseTool, input: String, metadata: [String: String]) throws {
+    open func on_tool_start(tool: BaseTool, input: String, metadata: [String: String]) throws {
         
     }
     
     // Chain callback
-    public func on_chain_end(output: String, metadata: [String: String]) throws {
+    open func on_chain_end(output: String, metadata: [String: String]) throws {
         
     }
     
-    public func on_chain_error(error: Error, metadata: [String: String]) throws {
+    open func on_chain_error(error: Error, metadata: [String: String]) throws {
         
     }
     
-    public func on_agent_action(action: AgentAction, metadata: [String: String]) throws {
+    open func on_agent_action(action: AgentAction, metadata: [String: String]) throws {
         
     }
     
-    public func on_agent_finish(action: AgentFinish, metadata: [String: String]) throws {
+    open func on_agent_finish(action: AgentFinish, metadata: [String: String]) throws {
         
     }
 
     
     // LLM callback
-    public func on_llm_new_token(metadata: [String: String]) {
+    open func on_llm_new_token(metadata: [String: String]) {
         
     }
     
-    public func on_llm_end(output: String, metadata: [String: String]) throws {
+    open func on_llm_end(output: String, metadata: [String: String]) throws {
         
     }
     
     // Tool callback
-    public func on_tool_end(tool: BaseTool, output: String, metadata: [String: String]) throws {
+    open func on_tool_end(tool: BaseTool, output: String, metadata: [String: String]) throws {
         
     }
     
-    public func on_tool_error(error: Error, metadata: [String: String]) throws {
+    open func on_tool_error(error: Error, metadata: [String: String]) throws {
         
     }
 }
